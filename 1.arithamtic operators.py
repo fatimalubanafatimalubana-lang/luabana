24d3eecd262ae1e5446c4ae8fe213d7b25716302
fatimalubanafatimalubana-lang <fatimalubanafatimalubana@gmail.com>
2026-01-21 19:37:29 +0530

    Add files via upload

---
+++ 1.arithamtic operators.py
@@ -0,0 +1,13 @@
+# +, -, /, %, //, ** etc...
+
+a = 5
+b = 2
+
+print('sum =',a+b)
+print('multiply =', a*b)
+print('division =',a/b)
+print('diffrent =',a-b)
+print('modulas =',a%b)
+print('square =',a**b)
+print('floor div =',a//b) # whole number only not decimal points
+
